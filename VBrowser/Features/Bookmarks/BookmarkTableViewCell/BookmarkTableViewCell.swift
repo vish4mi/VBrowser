717dd53258663f8f49a69c222066cdd9d6756661
Vishal Bhadade <vishal.bhadade@olacabs.com>
2020-09-10 23:25:07 +0530

    WIP: Added bookmark and tab cells

---
+++ VBrowser/Features/Bookmarks/BookmarkTableViewCell/BookmarkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookmarkTableViewCell.swift
+//  VBrowser
+//
+//  Created by Vishal Bhadade on 10/09/20.
+//  Copyright © 2020 VB. All rights reserved.
+//
+
+import UIKit
+
+class BookmarkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VBrowser/Features/Tabs/TabTableViewCell/TabTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TabTableViewCell.swift
+//  VBrowser
+//
+//  Created by Vishal Bhadade on 10/09/20.
+//  Copyright © 2020 VB. All rights reserved.
+//
+
+import UIKit
+
+class TabTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
